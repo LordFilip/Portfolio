7c9d1675dd57d64f7abc4b9715b434d6667173c0
Filip Ivanović <120279318+LordFilip@users.noreply.github.com>
2024-01-30 13:28:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -36,9 +36,16 @@ window.addEventListener("scroll", function () {
   const windowHeight = window.innerHeight;
   const scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
 
-  const lineWidth = scrollPercent * 2 + "%";
-  document.querySelector(".line").style.width = lineWidth;
+  // Calculate the line width based on the scroll percentage
+  let lineWidth = scrollPercent * 2;
+
+  // Ensure the line width does not exceed 100vw
+  lineWidth = Math.min(lineWidth, 100);
+
+  // Apply the width to the line element
+  document.querySelector(".line").style.width = lineWidth + "vw";
 });
+
 window.addEventListener("scroll", function () {
   const scrollTop = window.scrollY;
   const documentHeight = document.documentElement.scrollHeight;
@@ -48,8 +55,30 @@ window.addEventListener("scroll", function () {
   const lineWidth = 100 - scrollPercent + "%"; // Adjusting line width
   document.querySelector(".line-2").style.width = lineWidth;
 });
+
 const bidon = document.getElementById("x");
 const lavor = document.getElementById("lavor");
 bidon.addEventListener("click", function () {
   lavor.classList.toggle("menu-move");
 });
+
+const grid = document.querySelector(".grid");
+const gridContainer = document.querySelector(".grid-container");
+
+const obsCallback = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      grid.classList.add("newgrid");
+    } else {
+      grid.classList.remove("newgrid");
+    }
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.4,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(gridContainer);
